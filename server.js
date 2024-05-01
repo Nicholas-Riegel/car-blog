acbdca8ed200550eb5dc55919a15ee33ebf42b19
Nicholas <nicholaspriegel@gmail.com>
2024-05-01 17:06:54 -0400

    mvp complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const methodOverride = require('method-override')
 const port = 3000
 const Car = require('./models/car')
+const Post = require('./models/post')
 
 //----------------------------Middleware--------------------------------------------
 
@@ -21,6 +22,8 @@ mongoose.connection.on('connected', ()=>{
 })
 
 //----------------------------Routes--------------------------------------------
+
+// CAR ROUTES
 
 // GET	/cars	Read	index	Display a list of all cars.
 app.get('/cars', async (req, res)=>{
@@ -42,31 +45,68 @@ app.post('/cars', async (req, res)=>{
     res.redirect('/cars')
 })
 
-// GET	/blog/:id	Read	show	Display a specific car’s details.
-app.get('/car/:id', async (req, res) => {
+// GET	/car/:id	Read	show	Display a specific car’s details.
+app.get('/cars/:id', async (req, res) => {
     const car = await Car.findById(req.params.id)
-    res.render('showCar.ejs', {car})
+    const allPosts = await Post.find()
+    res.render('showCar.ejs', {car, allPosts})
 })
 
-// GET	/blog/:id/edit	Read	edit	Show a form to edit an existing car’s details.
-app.get('/car/:id/edit', async (req, res)=>{
+// GET	/car/:id/edit	Read	edit	Show a form to edit an existing car’s details.
+app.get('/cars/:id/edit', async (req, res)=>{
     const car = await Car.findById(req.params.id)
     res.render('editCar.ejs', {car})
 })
 
 // PUT	/blog/:id	Update	update	Update a specific car’s details.
-app.put('/car/:id', async (req, res)=>{
+app.put('/cars/:id', async (req, res)=>{
     await Car.findByIdAndUpdate(req.params.id, {
         name: req.body['car-name'],
         description: req.body['car-description']
     })
-    res.redirect(`/cars`)
+    res.redirect(`/cars/${req.params.id}`)
 })
 
-// DELETE	/blog/:id	Delete	delete	Remove a specific car from the list.
-app.delete('/car/:id', async (req, res)=>{
+// DELETE	/car/:id	Delete	delete	Remove a specific car from the list.
+app.delete('/cars/:id', async (req, res)=>{
     await Car.findByIdAndDelete(req.params.id)
     res.redirect('/cars')
+})
+
+
+// POST ROUTES
+
+// POST	/cars/:carId/posts	Create	Add a new post to the list.
+app.post('/cars/:carId/posts', async (req, res)=>{
+    await Post.create({
+        title: req.body['post-title'],
+        body: req.body['post-body']
+    })
+    res.redirect(`/cars/${req.params.carId}`)
+})
+
+// GET	/cars/:carId/posts/:postId/edit	Read	edit	Show a form to edit an existing car’s details.
+app.get('/cars/:carId/posts/:postId/edit', async (req, res)=>{
+    const post = await Post.findById(req.params.postId)
+    res.render('editPost.ejs', {
+        carId: req.params.carId,
+        post
+    })
+})
+
+// PUT	/cars/:carId/posts/:postId	Update	update	Update a specific car’s details.
+app.put('/cars/:carId/posts/:postId', async (req, res)=>{
+    await Post.findByIdAndUpdate(req.params.postId, {
+        title: req.body['post-title'],
+        body: req.body['post-body']
+    })
+    res.redirect(`/cars/${req.params.carId}`)
+})
+
+// DELETE	/cars/:carId/posts/:postId	Delete	delete	Remove a specific car from the list.
+app.delete('/cars/:carId/posts/:postId', async (req, res)=>{
+    await Post.findByIdAndDelete(req.params.postId)
+    res.redirect(`/cars/${req.params.carId}`)
 })
 
 //----------------------------Service--------------------------------------------
